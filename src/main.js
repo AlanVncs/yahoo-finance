1df4a346d385c2dad0ec59af1f6be2a1b0a9f3ba
Alan Vinicius Silva <alanvinicios@gmail.com>
2020-01-25 09:51:54 -0300

    Finalizado geração da tabela

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
-// 315619200 + milis
+const scraper = require("./scraper");
 
-console.log();
+const symbols = ['^BVSP', 'ALPA4', 'AMAR3', 'ARZZ3'];
+//, 'BBAS3', 'BBDC3', 'BBDC4', 'BOVA11', 'CAML3', 'CMIG4', 'COGN3', 'CPFE3', 'CPLE6', 'CSMG3', 'CSNA3', 'CYRE3', 'DTEX3', 'EGIE3', 'ENBR3', 'ENEV3', 'ENGI11', 'EQTL3', 'FESA4', 'FLRY3', 'GGBR4', 'GOAU3', 'GOLL4', 'GRND3', 'GUAR3', 'ITSA4', 'ITUB3', 'ITUB4', 'JHSF3', 'LREN3', 'MDIA3', 'MRVE3', 'MULT3', 'ODPV3', 'OFSA3', 'PARD3', 'QUAL3', 'RADL3', 'RENT3', 'SANB11', 'SAPR4', 'SBSP3', 'SEER3', 'SMTO3', 'TEND3', 'TRIS3', 'TRPL4', 'USIM3', 'VIVT4', 'VULC3', 'WEGE3', 'YDUQ3'];
+
+const table = [];
+var counter = 0;
+
+
+update();
+
+async function update(){
+    counter = 0;
+    symbols.forEach((symbol) => {
+        scraper(symbol).then(histPrice => {
+            histPrice.forEach(element => {
+                const date = element["date"];
+                if(!table[date]){
+                    table[date] = [];
+                }
+                table[date][symbol] = [];
+                table[date][symbol]["adj"] = element["adj"];
+            });
+            
+            counter++;
+            
+            // last call back
+            if(counter >= symbols.length){
+                // TODO Gerar planilhas
+            }
+        });
+    });
+}
---
+++ src/scraper.js
@@ -0,0 +1,73 @@
+const puppeteer = require('puppeteer');
+const strToStream = require('string-to-stream')
+const csv = require('csv-parser');
+
+const PERIOD = 315619200; // 10 years in seconds
+
+module.exports = async function scrapData(symbol){
+
+    if(!symbol) return null;
+
+    symbol = (symbol==="^BVSP")?symbol:`${symbol}.SA`;
+
+    const url = buildURL(symbol);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url);
+    await page.waitForSelector(`#quote-header-info > div + div + div > div > div > span`);
+    await page.waitForSelector(`a[download='${symbol}.csv`);
+
+    try{
+        const {csvHistPrice, currentPrice} =  await page.evaluate(async (symbol) => {
+            var currentPrice = document.querySelector("#quote-header-info > div + div + div > div > div > span").innerText; // Price now
+            var fileLink = document.querySelector(`a[download='${symbol}.csv']`).href; // CSV file link with historical data
+            const csvHistPrice = await (await fetch(fileLink, {credentials: 'include'})).text(); // Download CSV file as string (Not necessarily includes today's price)
+            return {csvHistPrice, currentPrice};
+        }, symbol);
+
+        return buildFromCSV(csvHistPrice, currentPrice);
+    }
+    catch(error){
+        // On rare occasions loading the page doesn't works
+        // Try again is sufficient
+        // console.log(`Error ${symbol}: ${error.message}`);
+        return scrapData(symbol);
+    }
+    finally{
+        browser.close();
+    }
+}
+
+async function buildFromCSV(csvHistPrice, currentPrice){
+    return new Promise((resolve, reject) => {
+        const histPrice = [];
+        strToStream(csvHistPrice).pipe(csv(['date', 'open', 'high', 'low', 'close', 'adj', 'volume']))
+        .on('data', (element) => histPrice.push(element))
+        .on('end', () => {
+            histPrice.shift();
+            histPrice.push({date: today(), adj: currentPrice});
+            resolve(histPrice);
+        });
+    });
+}
+
+function buildURL(symbol){
+    const end = Math.floor(Date.now()/1000) - 30; // -30 seconds to be sure it isn't future ;D
+    const start = end - PERIOD;
+    return encodeURI(`https://finance.yahoo.com/quote/${symbol}/history?period1=${start}&period2=${end}&interval=1d&filter=history&frequency=1d`);
+}
+
+function today(){
+    const now = new Date();
+
+    var year = now.getUTCFullYear();
+
+    var month = now.getUTCMonth() + 1;
+    month = (month < 10)?`0${month}`:month;
+
+    var date = now.getUTCDate();
+    date = (date<10)?`0${date}`:date;
+
+    return `${year}-${month}-${date}`;
+}

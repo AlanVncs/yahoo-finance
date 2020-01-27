b794f0b57fddfb672d07d4291620391b43134b7b
Alan Vinicius Silva <alanvinicios@gmail.com>
2020-01-26 22:52:31 -0300

    Baixando dados e  movendo arquivos para o diretório de downloads corretamente

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 const fs = require('fs');
 const scraper = require("./scraper");
 
-const symbols = ['^BVSP', 'ALPA4', 'AMAR3', 'ARZZ3', 'BBAS3'];
-//, 'BBDC3', 'BBDC4', 'BOVA11', 'CAML3', 'CMIG4', 'COGN3', 'CPFE3', 'CPLE6', 'CSMG3', 'CSNA3', 'CYRE3', 'DTEX3', 'EGIE3', 'ENBR3', 'ENEV3', 'ENGI11', 'EQTL3', 'FESA4', 'FLRY3', 'GGBR4', 'GOAU3', 'GOLL4', 'GRND3', 'GUAR3', 'ITSA4', 'ITUB3', 'ITUB4', 'JHSF3', 'LREN3', 'MDIA3', 'MRVE3', 'MULT3', 'ODPV3', 'OFSA3', 'PARD3', 'QUAL3', 'RADL3', 'RENT3', 'SANB11', 'SAPR4', 'SBSP3', 'SEER3', 'SMTO3', 'TEND3', 'TRIS3', 'TRPL4', 'USIM3', 'VIVT4', 'VULC3', 'WEGE3', 'YDUQ3'];
+const symbols = ['^BVSP', 'ALPA4', 'AMAR3', 'ARZZ3', 'BBAS3', 'BBDC3', 'BBDC4', 'BOVA11', 'CAML3', 'CMIG4', 'COGN3', 'CPFE3', 'CPLE6', 'CSMG3', 'CSNA3', 'CYRE3', 'DTEX3', 'EGIE3', 'ENBR3', 'ENEV3', 'ENGI11', 'EQTL3', 'FESA4', 'FLRY3', 'GGBR4', 'GOAU3', 'GOLL4', 'GRND3', 'GUAR3', 'ITSA4', 'ITUB3', 'ITUB4', 'JHSF3', 'LREN3', 'MDIA3', 'MRVE3', 'MULT3', 'ODPV3', 'OFSA3', 'PARD3', 'QUAL3', 'RADL3', 'RENT3', 'SANB11', 'SAPR4', 'SBSP3', 'SEER3', 'SMTO3', 'TEND3', 'TRIS3', 'TRPL4', 'USIM3', 'VIVT4', 'VULC3', 'WEGE3', 'YDUQ3'];
 
 var table = [];
 
-var threads = 3;
+var threads = 10; // Tested on a Ryzen 3 2200G with 8GB RAM
 var started = 0;
 var finished = 0;
 
@@ -25,14 +24,19 @@ async function scrapData(symbol){
 
     started++;
     console.log(`Started: ${started}/${symbols.length}`);
-    scraper(symbol).then((histPrice) => {
+    scraper(symbol).then(async (histPrice) => {
         histPrice.forEach(element => {
             const date = element["date"];
             if(!table[date]){
                 table[date] = [];
             }
             table[date][symbol] = [];
+            table[date][symbol]["open"] = (element['open'] == 'null')?'FALSO':element['open'];
+            table[date][symbol]["high"] = (element['high'] == 'null')?'FALSO':element['high'];
+            table[date][symbol]["low"] = (element['low'] == 'null')?'FALSO':element['low'];
+            table[date][symbol]["close"] = (element['close'] == 'null')?'FALSO':element['close'];
             table[date][symbol]["adj"] = (element['adj'] == 'null')?'FALSO':element['adj'];
+            table[date][symbol]["volume"] = (element['volume'] == 'null')?'FALSO':element['volume'];
         });
         
         finished++;
@@ -42,7 +46,8 @@ async function scrapData(symbol){
         
         if(finished == symbols.length){ // last symbol
             sortTable();
-            writeCSV();
+            await writeCSV();
+            moveFiles();
             return;
         }
     });
@@ -56,34 +61,94 @@ function sortTable(){
     table = newTable;
 }
 
-function writeCSV(){
+async function writeCSV(){
+
     const tmpFilesDir = './tmpFiles';
-    if(!fs.existsSync(tmpFilesDir)){
-        fs.mkdirSync(tmpFilesDir);
-    }
-    const adjStream = fs.createWriteStream(`${tmpFilesDir}/adj.csv`);
 
-    // Headers
-    adjStream.write('Data');
+    const streams  = {
+        open: fs.createWriteStream(`${tmpFilesDir}/open.csv`),
+        high: fs.createWriteStream(`${tmpFilesDir}/high.csv`),
+        low: fs.createWriteStream(`${tmpFilesDir}/low.csv`),
+        close: fs.createWriteStream(`${tmpFilesDir}/close.csv`),
+        adj: fs.createWriteStream(`${tmpFilesDir}/adj.csv`),
+        volume: fs.createWriteStream(`${tmpFilesDir}/volume.csv`),
+        write: writeStreams,
+        end: endStreams
+    };
+
+    var v = streams['open'];
+
+    // Write headers
+    streams.write('Data');
     symbols.forEach(symbol => {
-        adjStream.write(`;${symbol}`);
+        streams.write(`;${symbol}`);
     });
-    adjStream.write('\n');
+    streams.write('\n');
 
 
-    // Values
+    // Write date + values
     for(date in table){
-        adjStream.write(date);
+        streams.write(date);
         symbols.forEach(symbol => {
-            value = (table[date][symbol])?table[date][symbol]['adj']:'FALSO';
-            adjStream.write(`;${value}`);
+            streams.write(';');
+            streams.write(table[date][symbol], true);
         });
-        adjStream.write('\n');
+        streams.write('\n');
     }
 
-    adjStream.close();
+    // Promise resolved when all stream are properly writen and closed
+    return streams.end();
+}
+
+function writeStreams(content, flag){
+    const names = ['open', 'high', 'low', 'close', 'adj', 'volume'];
+    if(flag){
+        names.forEach((name)=>{
+            this[name].write(content && content[name] || 'FALSO');
+        });
+    }
+    else{
+        names.forEach((name)=>{
+            this[name].write(content);
+        });
+    }
+}
+
+async function endStreams(){
+    // this: [key => Stream]
+    const names = ['open', 'high', 'low', 'close', 'adj', 'volume'];
+    const promises = [];
+    names.forEach((name) => {
+        promises.push(endStream(this[name]));
+    });
+    return Promise.all(promises);
+}
+
+async function endStream(stream){
+    return new Promise((resolve, reject) => {
+        stream.end(resolve);
+    });
 }
 
 function moveFiles(){
+    const fileDir = './files';
+    const bkpDir = `${fileDir}/backups`;
+    const tmpDir = './tmpFiles';
+    const names = ['open', 'high', 'low', 'close', 'adj', 'volume'];
+    // const adj = 'adj.csv'
     
+    // Backup previous file
+    names.forEach((name) => {
+        if(fs.existsSync(`${fileDir}/${name}.csv`)){
+            fs.copyFileSync(`${fileDir}/${name}.csv`, `${bkpDir}/${name}.csv`);
+        }
+    
+        try{
+            fs.copyFileSync(`${tmpDir}/${name}.csv`, `${fileDir}/${name}.csv`);
+        }
+        catch(error){
+            console.log(`Error while copying file ${tmpDir}/${name}.csv to ${fileDir}/${name}.csv`);
+            console.log(error);
+        }
+    });
 }

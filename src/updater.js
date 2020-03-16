c649de47f9677cd2fecd54a93b762f1daae607ce
Alan Vinicius Silva <alanvinicios@gmail.com>
2020-03-15 21:40:53 -0300

    Symbols adicionados

--- src/updater.js
+++ src/updater.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const scraper = require("./scraper");
 
-const symbols = ['^BVSP', 'ALPA4', 'AMAR3', 'ARZZ3', 'BBAS3', 'BBDC3', 'BBDC4', 'BOVA11', 'CAML3', 'CMIG4', 'COGN3', 'CPFE3', 'CPLE6', 'CSMG3', 'CSNA3', 'CYRE3', 'DTEX3', 'EGIE3', 'ENBR3', 'ENEV3', 'ENGI11', 'EQTL3', 'FESA4', 'FLRY3', 'GGBR4', 'GOAU3', 'GOLL4', 'GRND3', 'GUAR3', 'ITSA4', 'ITUB3', 'ITUB4', 'JHSF3', 'LREN3', 'MDIA3', 'MRVE3', 'MULT3', 'ODPV3', 'OFSA3', 'PARD3', 'QUAL3', 'RADL3', 'RENT3', 'SANB11', 'SAPR4', 'SBSP3', 'SEER3', 'SMTO3', 'TEND3', 'TRIS3', 'TRPL4', 'USIM3', 'VIVT4', 'VULC3', 'WEGE3', 'YDUQ3'];
+const symbols = ['^BVSP', 'ALPA4', 'AMAR3', 'ARZZ3', 'BBAS3', 'BBDC3', 'BBDC4', 'BOVA11', 'CAML3', 'CMIG4', 'COGN3', 'CPFE3', 'CPLE6', 'CSMG3', 'CSNA3', 'CYRE3', 'DIVO11', 'DTEX3', 'EGIE3', 'ENBR3', 'ENEV3', 'ENGI11', 'EQTL3', 'FESA4', 'FIND11', 'FLRY3', 'GGBR4', 'GOAU3', 'GOLL4', 'GRND3', 'GUAR3', 'ITSA4', 'ITUB3', 'ITUB4', 'IVVB11', 'JHSF3', 'LCAM3', 'LREN3', 'MDIA3', 'MOVI3', 'MRVE3', 'MULT3', 'ODPV3', 'OFSA3', 'PARD3', 'PRIO3', 'QUAL3', 'RADL3', 'RENT3', 'SANB11', 'SAPR4', 'SBSP3', 'SEER3', 'SMAL11', 'SMTO3', 'TEND3', 'TRIS3', 'TRPL4', 'UNID3', 'USIM3', 'VIVT4', 'VULC3', 'WEGE3', 'YDUQ3'];
 
 module.exports = async function update(callback){
     const table = [];
